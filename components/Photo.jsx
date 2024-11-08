3ef6e0ea70bb5210eac091b161690cb71907eed4
Kaushik Gupta <146950347+Kaushikgupta469@users.noreply.github.com>
2024-11-08 23:09:38 +0530

    Update Photo.jsx

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -3,66 +3,66 @@
 import { motion } from "framer-motion";
 import Image from "next/image";
 
-
-
 const Photo = () => {
   return (
-    <div className="relative w-full h-full">
+    <div className="relative w-full h-full flex items-center justify-center">
       <motion.div
-      initial={{opacity:0}}
-      animate={{
-        opacity: 1,
-        transition: {delay:2, duration: 0.4, ease: "easeIn"},
-      }}
+        initial={{ opacity: 0 }}
+        animate={{
+          opacity: 1,
+          transition: { delay: 2, duration: 0.4, ease: "easeIn" },
+        }}
       >
-      {/* image*/}
-        <motion.div 
-        initial={{opacity:0}}
-      animate={{
-        opacity: 1,
-        transition: {delay:2.4, duration: 0.4, ease: "easeInOut"},
-      }} 
-        className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px]
-        mix-blend-lighten absolute">
-            <Image 
-            src="/assets/photo.jpg" 
-            priority quality={100}
-            fill 
-            alt="" 
-            className="relative object-contain" />
+        {/* Image */}
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{
+            opacity: 1,
+            transition: { delay: 2.4, duration: 0.4, ease: "easeInOut" },
+          }}
+          className="w-[298px] h-[298px] xl:w-[500px] xl:h-[500px] rounded-full overflow-hidden"
+        >
+          <Image
+            src="/assets/photo.jpg"
+            priority
+            quality={100}
+            width={500}
+            height={500}
+            alt=""
+            className="object-cover"
+          />
         </motion.div>
 
-        {/*circle*/}
+        {/* Circle */}
         <motion.svg
-        className="w-[300px] xl:w-[506px] h-[300px] xl:h-[506px]"
-        fill="transparent"
-        viewBox="0 0 506 506"
-        xmlns="http://www.w3.org/2000/svg"
+          className="absolute w-[300px] xl:w-[506px] h-[300px] xl:h-[506px]"
+          fill="transparent"
+          viewBox="0 0 506 506"
+          xmlns="http://www.w3.org/2000/svg"
         >
-            <motion.circle 
-            cx="255" 
-            cy="244" 
-            r="228" 
-            stroke="#8289f7" 
-            strokeWidth="4" 
+          <motion.circle
+            cx="255"
+            cy="255"
+            r="228"
+            stroke="#8289f7"
+            strokeWidth="4"
             strokeLinecap="round"
             strokeLinejoin="round"
-            initial={{strokeDasharray:"24 10 0 0"}}
+            initial={{ strokeDasharray: "24 10 0 0" }}
             animate={{
               strokeDasharray: ["15 120 25 25", "16 25 92 72", "4 250 22 22"],
-              rotate:[120,360],  
+              rotate: [120, 360],
             }}
             transition={{
-                duration: 20,
-                repeat: Infinity,
-                repeatType: "reverse",
+              duration: 20,
+              repeat: Infinity,
+              repeatType: "reverse",
             }}
-           
-            />
+          />
         </motion.svg>
       </motion.div>
     </div>
   );
-}
+};
 
 export default Photo;
