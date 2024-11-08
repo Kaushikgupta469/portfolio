1006d5fab400340eda9b851247290aa6c2333dba
Kaushik Gupta <146950347+Kaushikgupta469@users.noreply.github.com>
2024-11-08 23:23:08 +0530

    Update Photo.jsx

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 
 const Photo = () => {
   return (
-    <div className="relative w-[500px] h-[500px] flex items-center justify-center">
+    <div className="relative w-[300px] h-[300px] flex items-center justify-center">
       <motion.div
         initial={{ opacity: 0 }}
         animate={{
@@ -14,14 +14,14 @@ const Photo = () => {
         }}
         className="relative flex items-center justify-center"
       >
-        {/* image */}
+        {/* Image */}
         <motion.div
           initial={{ opacity: 0 }}
           animate={{
             opacity: 1,
             transition: { delay: 2.4, duration: 0.4, ease: "easeInOut" },
           }}
-          className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] absolute z-10"
+          className="w-[300px] h-[300px] absolute z-10"
         >
           <Image
             src="/assets/photo.jpg"
@@ -33,17 +33,17 @@ const Photo = () => {
           />
         </motion.div>
 
-        {/* animated circle */}
+        {/* Animated Circle */}
         <motion.svg
-          className="absolute w-[300px] xl:w-[506px] h-[300px] xl:h-[506px] z-20"
+          className="absolute w-[300px] h-[300px] z-20"
           fill="transparent"
-          viewBox="0 0 506 506"
+          viewBox="0 0 300 300"
           xmlns="http://www.w3.org/2000/svg"
         >
           <motion.circle
-            cx="255"
-            cy="244"
-            r="228"
+            cx="150"
+            cy="150"
+            r="148"
             stroke="#8289f7"
             strokeWidth="4"
             strokeLinecap="round"
